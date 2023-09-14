7f01498cd2f0114476f0d37ff0ca3d447d180940
Hitesh Regar <haikent@Hiteshs-Mac-mini.local>
2023-09-15 00:22:56 +0530

    ++

--- search.php
+++ search.php
@@ -21,7 +21,7 @@ try {
     // $stmt->bindValue(':pattern', $pattern, SQLITE3_TEXT);
 
     // Query to retrieve the first two rows
-    $query = "SELECT * FROM pdf_text_data where raw_text_data like '*खोज*' ";
+    $query = "SELECT * FROM pdf_text_data where raw_text_data REGEXP '/^.*खोज.*$/i' ";
     
     $result = $db->query($query);
 
