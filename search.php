76d3c7f0352f4c349c503196f2b795c8c57ec1be
Hitesh Regar <haikent@Hiteshs-Mac-mini.local>
2023-09-15 00:16:00 +0530

    ++

--- search.php
+++ search.php
@@ -23,7 +23,7 @@ try {
     // $stmt->bindValue(':pattern', $pattern, SQLITE3_TEXT);
 
     // Query to retrieve the first two rows
-    $query = "SELECT * FROM my_table WHERE raw_text_data like प्ररन";
+    $query = "SELECT * FROM my_table WHERE raw_text_data limit 5";
     $result = $db->query($query);
 
     // Check if the query was successful
