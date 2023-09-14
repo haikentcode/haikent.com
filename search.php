2d09e908b594b27d18fe8fff2ffc149acc24913e
Hitesh Regar <haikent@Hiteshs-Mac-mini.local>
2023-09-14 23:59:38 +0530

    ++

--- search.php
+++ search.php
@@ -1,15 +1,42 @@
 <?php
-
-
 try {
-  $db = new SQLite3('pdf_text.db');
-  if ($db) {
-      echo "Connected to the database.";
-  } else {
-      echo "Failed to connect to the database.";
-  }
-} catch (Exception $e) {
-  echo "An error occurred: " . $e->getMessage();
-}
+    $db = new SQLite3('pdf_text.db');
 
+    // Check if the database connection was successful
+    if (!$db) {
+        echo "Failed to connect to the database.";
+        exit();
+    }
+
+    // Query to retrieve the first two rows
+    $query = "SELECT * FROM my_table LIMIT 2";
+    $result = $db->query($query);
+
+    // Check if the query was successful
+    if ($result) {
+        echo "<html><head><title>Table Data</title></head><body>";
+        echo "<h1>Table Data</h1>";
+        echo "<table border='1'>";
+        echo "<tr><th>ID</th><th>Name</th><th>Description</th></tr>";
+
+        // Loop through the result set and print each row
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['name'] . "</td>";
+            echo "<td>" . $row['description'] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+        echo "</body></html>";
+    } else {
+        echo "Query failed.";
+    }
+
+    // Close the database connection
+    $db->close();
+} catch (Exception $e) {
+    echo "An error occurred: " . $e->getMessage();
+}
 ?>
