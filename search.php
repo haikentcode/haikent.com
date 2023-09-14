c8e0d011c0f615e3f0823cd03011636d163799fb
Hitesh Regar <haikent@Hiteshs-Mac-mini.local>
2023-09-14 23:46:56 +0530

    ++

--- search.php
+++ search.php
@@ -5,7 +5,15 @@ phpinfo();
       $db = new SQLite3('pdf_text.db');
 
       if (isset($_GET['query'])) {
+
             $searchQuery = $_GET['query'];
+
+            $sql = "SELECT pdf_name, page_number, raw_text_data FROM pdf_text_data WHERE raw_text_data LIKE :query";
+            $stmt = $db->prepare(sql);
+            $stmt->bindValue(':query', '%' . $searchQuery . '%', SQLITE3_TEXT);
+
+            // Execute the query and fetch results
+            $results = $stmt->execute();
 
             echo "Hello search ->".$searchQuery;
 
