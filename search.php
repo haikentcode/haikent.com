4d04faea2aaf33e95091e85de61dfdf13a948479
Hitesh Regar <haikent@Hiteshs-Mac-mini.local>
2023-09-15 00:26:02 +0530

    ++

--- search.php
+++ search.php
@@ -8,34 +8,74 @@ try {
         exit();
     }
 
-    $searchQuery = $_GET['query'];
-    $line = preg_quote($searchQuery, '/');
+    // Get the Hindi text line from the query parameter
+    $hindiLine = isset($_GET['query']) ? $_GET['query'] : '';
 
-    // Construct the regex pattern to match the exact line in the description column
-    $pattern = '/^' . $line . '$/i'; // Use the "i" flag for case-insensitivity
+    // Split the Hindi text line into individual words
+    $hindiWords = preg_split('/\s+/', $hindiLine, -1, PREG_SPLIT_NO_EMPTY);
 
+    // Create an array to store the results with scores
+    $results = array();
 
+    // Loop through each word in the Hindi text line
+    foreach ($hindiWords as $hindiWord) {
+        // Construct the regex pattern with the Hindi word
+        $pattern = '/\b' . preg_quote($hindiWord, '/') . '\b/i'; // Case-insensitive search
 
-    // $query = "SELECT * FROM pdf_text_data WHERE raw_text_data REGEXP :pattern";
-    // $stmt = $db->prepare($query);
-    // $stmt->bindValue(':pattern', $pattern, SQLITE3_TEXT);
+        // Query to search for rows containing the specified Hindi word in the raw_text_data column
+        $query = "SELECT * FROM pdf_text_data WHERE raw_text_data REGEXP :pattern";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':pattern', $pattern, SQLITE3_TEXT);
 
-    // Query to retrieve the first two rows
-    $query = "SELECT * FROM pdf_text_data where raw_text_data REGEXP '/^.*खोज.*$/i' ";
-    
-    $result = $db->query($query);
+        // Execute the query
+        $result = $stmt->execute();
 
-    // Check if the query was successful
-    if ($result) {
+        // Check if the query was successful
+        if ($result) {
+            // Loop through the result set and calculate a score for each result
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                // Calculate a score based on the number of matches in the raw_text_data
+                $score = preg_match_all($pattern, $row['raw_text_data'], $matches);
 
-        // Loop through the result set and print each row
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            echo $row['raw_text_data']."</br>";
+                // Store the result with the score
+                $results[] = array(
+                    'pdf_name' => $row['pdf_name'],
+                    'page_number' => $row['page_number'],
+                    'raw_text_data' => $row['raw_text_data'],
+                    'score' => $score,
+                );
+            }
+        }
+    }
+
+    // Sort the results by score in descending order (higher score first)
+    usort($results, function ($a, $b) {
+        return $b['score'] - $a['score'];
+    });
+
+    // Display the results with scores
+    echo "<html><head><title>Search Results</title></head><body>";
+    echo "<h1>Search Results</h1>";
+
+    if (!empty($results)) {
+        echo "<table border='1'>";
+        echo "<tr><th>PDF Name</th><th>Page Number</th><th>Raw Text Data</th><th>Score</th></tr>";
+
+        foreach ($results as $result) {
+            echo "<tr>";
+            echo "<td>" . $result['pdf_name'] . "</td>";
+            echo "<td>" . $result['page_number'] . "</td>";
+            echo "<td>" . $result['raw_text_data'] . "</td>";
+            echo "<td>" . $result['score'] . "</td>";
+            echo "</tr>";
         }
 
+        echo "</table>";
     } else {
-        echo "Query failed.";
+        echo "No matching rows found.";
     }
+
+    echo "</body></html>";
 
     // Close the database connection
     $db->close();
